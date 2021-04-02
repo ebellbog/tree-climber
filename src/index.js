2fc2918183db24be14660fddd648b96f2431b4f7
Elana Bell Bogdan <e.bell.bogdan@gmail.com>
2021-04-02 17:32:21 -0400

    add bishops to boards

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import './index.less';
 
-const isMobile = () => Boolean(window.matchMedia("only screen and (max-device-width: 850px)").matches);
-
-// Load assets dynamically
-function loadAsset(el$, asset, attr = 'src') {
-    el$.attr(attr, `${asset}`);
-}
-
-function randInt(min, max) {
-    return Math.round(Math.random() * (max - min)) + min;
-}
-
 // Important: keep in sync with values in index.less
 const NUM_ROWS = 4; 
 const NUM_COLS = 5;
 
 $(document).ready(() => {
+    setupBoards();
+});
+
+function setupBoards() {
     const $board = $('.board');
     for (let i = 0; i < NUM_ROWS * NUM_COLS; i++) {
         const $square = $('<div></div>').addClass('square');
+        const $bishop = $('<i class="fa fa-chess-bishop"></i>');
+
+        const colIdx = i % NUM_COLS;
+        if (colIdx === 0 || colIdx === NUM_COLS - 1) {
+            $square.append($bishop.clone().addClass(colIdx === 0 ? 'black' : 'white'));
+        }
         $board.append($square);
     }
-});
+
+}
